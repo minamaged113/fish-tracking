ec3510f1b66ae5db90883e7c9eaf3de4ed3653dd
MinaLinuxWorkNewInstallation <minamaged113@gmail.com>
2018-07-30 15:51:15 +0300

    working on UI

--- UI/UI_utils.py
+++ UI/UI_utils.py
@@ -3,8 +3,9 @@ from tkinter import filedialog
 
 fileTypeTuple = ( ("ARIS files", "*.aris"), ("All files", "*.*"))
 
-def getFilePath(root):
+def openFile(root):
     root.fileName = filedialog.askopenfilename(filetypes = fileTypeTuple)
+    # root.show_frame[]
     return root.fileName
 
 def show_frame():
