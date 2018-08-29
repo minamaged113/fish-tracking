3d5adb917a41fc94aa2eef2afd988389ef42bcbc
minaLenovo <minamaged113@gmail.com>
2018-08-29 19:44:48 +0300

    working with the UI

--- test.py
+++ test.py
@@ -20,145 +20,152 @@ from skimage.transform import rescale
 #       UI elements
 ##########################################################
 
-class FMainWindow(QMainWindow):
-    """This class holds the main window which will be used to 
-    show the SONAR images, analyze them and edit images.
-    
-    Arguments:
-        QMainWindow {Class} -- inheriting from QDialog class.
+def FGetIcon(folder, icon):
+    ## TODO
+    """get icon path.
     """
 
-    UI_FRAME_INDEX = 0
-    FRAMES_LIST = list()
-    SCALE = 2.0/3.0
+    pass
+
+class FWelcomeScreen(QMainWindow):
+    """This class holds the welcome window which will be used to 
+    open ARIS and DIDSON files and show statistics and information
+    about the project and the developers.
+    
+    Arguments:
+        QMainWindow {Class} -- inheriting from QMainWindow class.
+    """
     def __init__(self):
-        """Initializes the window and loads the first frame and
-        places the UI elements, each in its own place.
+        """Initializes the window and displays brief information
+        about LUKE and University of Oulu.
         """
-        ##  Reading the file
-        self.FLoadARISFile()
         ##  UI elements description
         QMainWindow.__init__(self)
-        self.setWindowTitle("Fisher - " + self.FFilePath)
-        self.FLayout = QGridLayout()
-
-        FNextBTN = QPushButton("Next",self)
-        FNextBTN.clicked.connect(self.FShowNextImage)
-        FPreviousBTN = QPushButton("Previous",self)
-        FPreviousBTN.clicked.connect(self.FShowPreviousImage)
-
-        self.FLayout.addWidget(FNextBTN,1,1)
-        self.FLayout.addWidget(FPreviousBTN,1,0)
-       
-        self.Frame = self.File.readFrame(self.UI_FRAME_INDEX)
-        print(self.UI_FRAME_INDEX)
-        self.qformat =QImage.Format_Grayscale8
-        self.Frame.IMAGE = rescale(self.Frame.IMAGE, self.SCALE, anti_aliasing=True)
-        self.Frame.IMAGE = (self.Frame.IMAGE*255).astype(np.uint8)
+        self.initUI()
+        self.FCentralScreen = FMainWindow(self)
+        self.setCentralWidget(self.FCentralScreen)
         
-        self.image = QImage(self.Frame.IMAGE,
-                            self.Frame.IMAGE.shape[1],
-                            self.Frame.IMAGE.shape[0],
-                            self.Frame.IMAGE.strides[0],
-                            self.qformat)
-        self.imageLabel = QLabel()
-        self.imageLabel.setPixmap(QPixmap.fromImage(self.image))
+
+    def initUI(self):
+        self.setWindowIcon(QIcon("/home/mghobria/Desktop/fish-tracking/UI/UI_presentation/salmon/salmon_16"))
+        self.setWindowTitle("Fisher - Welcome Screen")
+
+        self.FMainMenu_init()        
+        self.show()
+
         
-        self.FLayout.addWidget(self.imageLabel,0,0,1,2, Qt.AlignCenter)
-
-        self.setLayout(self.FLayout)
-
-
-
-    def FShowNextImage(self):
-        """Show the next frame image.
+    def FMainMenu_init(self):
+        """initializes the main menu for the application.
         """
+        self.mainMenu = self.menuBar()
+        self.FFileMenu_init()
+        self.FEditMenu_init()
+        self.FHelpMenu_init()
+        self.statusBar()
+        return
 
-        self.UI_FRAME_INDEX +=1
-        print(self.UI_FRAME_INDEX)
-        if (self.UI_FRAME_INDEX > self.File.frameCount-1):
-            self.UI_FRAME_INDEX = 0
-        self.Frame = self.File.readFrame(self.UI_FRAME_INDEX)
-        self.qformat =QImage.Format_Grayscale8
-        self.Frame.IMAGE = rescale(self.Frame.IMAGE, self.SCALE, anti_aliasing=True)
-        self.Frame.IMAGE = (self.Frame.IMAGE*255).astype(np.uint8)
+    def FFileMenu_init(self):
+        ## TODO : add signal handler to open user files
+        openFileAction = QAction("Open File", self)
+        openFileAction.setShortcut("Ctrl+O")
+        openFileAction.setStatusTip("Loads a new file to the program.")
+        openFileAction.triggered.connect(self.FOpenFile)
+
+        ## TODO : load a folder for a day processing
+        openFolderAction = QAction("Open Folder", self)
+        openFolderAction.setShortcut("Ctrl+Shift+O")
+        openFolderAction.setStatusTip("Opens a whole folder and loads it to the program.")
+        openFolderAction.triggered.connect(lambda : self.print_stat_msg("Open Folder pressed."))
+
+        ## TODO : add signal handler to save file after editing.
+        saveFileAction = QAction("Save", self)
+        saveFileAction.setShortcut("Ctrl+S")
+        saveFileAction.setStatusTip("Saves the current file.")
+        saveFileAction.triggered.connect(lambda : self.print_stat_msg("save file pressed."))
+
+        ## TODO : add signal handler to save file as new file 
+        saveFileAsAction = QAction("Save as ...", self)
+        saveFileAsAction.setShortcut("Ctrl+Shift+S")
+        saveFileAsAction.setStatusTip("Saves current work as new file.")
+        saveFileAsAction.triggered.connect(lambda : self.print_stat_msg("Save file as pressed."))
+
+        exitAction = QAction("Exit", self)
+        exitAction.setShortcut("Ctrl+Q")
+        exitAction.setStatusTip("Exits the application.")
+        exitAction.triggered.connect(QCoreApplication.instance().quit)
+
+        fileMenu = self.mainMenu.addMenu("&File")
+        fileMenu.addAction(openFileAction)
+        fileMenu.addAction(openFolderAction)
+        fileMenu.addAction(saveFileAction)
+        fileMenu.addAction(saveFileAsAction)
+        fileMenu.addSeparator()
+        fileMenu.addAction(exitAction)
+
+    def FHelpMenu_init(self):
+        ## TODO : add signal handler to open webpage
+        aboutAction = QAction("About", self)
+        aboutAction.setStatusTip("Opens a webpage contains all info about the project.")
+        aboutAction.triggered.connect(lambda: self.print_stat_msg("About pressed"))
+
+        ## TODO : add signal handler to check for updates
+        checkForUpdatesAction = QAction("Check for updates", self)
+        checkForUpdatesAction.setStatusTip("Check for updates online")
+        checkForUpdatesAction.triggered.connect(lambda: self.print_stat_msg("Check for updates pressed."))
+
+        ## TODO : add signal handler to show license file.
+        viewLicenseAction = QAction("View License", self)
+        viewLicenseAction.setStatusTip("Shows the licenses for the whole software.")
+        viewLicenseAction.triggered.connect(lambda: self.print_stat_msg("view license pressed."))
+
+        ## TODO : add signal handler to report an issue with the software
+        reportAction = QAction("Report Issue", self)
+        reportAction.setStatusTip("Report an issue to the developers.")
+        reportAction.triggered.connect(lambda : self.print_stat_msg("reprot issure pressed."))
         
-        self.image = QImage(self.Frame.IMAGE,
-                            self.Frame.IMAGE.shape[1],
-                            self.Frame.IMAGE.shape[0],
-                            self.Frame.IMAGE.strides[0],
-                            self.qformat)
-        self.imageLabel = QLabel()
-        self.imageLabel.setPixmap(QPixmap.fromImage(self.image))
-        
-        self.FLayout.addWidget(self.imageLabel,0,0,1,2, Qt.AlignCenter)
+        ## TODO : add signal handler to show statistics
+        showStatisticsAction = QAction("Statistics", self)
+        showStatisticsAction.setStatusTip("Shows statistics about old processed files.")
+        showStatisticsAction.triggered.connect(lambda : self.print_stat_msg("Statistics pressed."))
 
 
-    def FShowPreviousImage(self):
-        """Show the previous frame image
-        """
+        helpMenu = self.mainMenu.addMenu("&Help")
+        helpMenu.addAction(showStatisticsAction)
+        helpMenu.addSeparator()
+        helpMenu.addAction(reportAction)
+        helpMenu.addAction(viewLicenseAction)
+        helpMenu.addAction(checkForUpdatesAction)
+        helpMenu.addSeparator()
+        helpMenu.addAction(aboutAction)
 
-        self.UI_FRAME_INDEX -= 1
-        print(self.UI_FRAME_INDEX)
-        if (self.UI_FRAME_INDEX < 0 ):
-            self.UI_FRAME_INDEX = self.File.frameCount-1
-        self.Frame = self.File.readFrame(self.UI_FRAME_INDEX)
-        self.qformat =QImage.Format_Grayscale8
-        self.Frame.IMAGE = rescale(self.Frame.IMAGE, self.SCALE, anti_aliasing=True)
-        self.Frame.IMAGE = (self.Frame.IMAGE*255).astype(np.uint8)
-        
-        self.image = QImage(self.Frame.IMAGE,
-                            self.Frame.IMAGE.shape[1],
-                            self.Frame.IMAGE.shape[0],
-                            self.Frame.IMAGE.strides[0],
-                            self.qformat)
-        self.imageLabel = QLabel()
-        self.imageLabel.setPixmap(QPixmap.fromImage(self.image))
-        
-        self.FLayout.addWidget(self.imageLabel,0,0,1,2, Qt.AlignCenter)
 
-    def FLoadARISFile(self):
-        self.FFilePath = "/home/mghobria/Documents/work/data/data.aris"
-        # self.FFilePath = "/home/mghobria/Desktop/sample.aris"
-        self.File = ARIS_File.ARIS_File(self.FFilePath)
-        if(self.File.fileVersion()):
-            print("file loaded successfully")
-        else:
-            print("some error happened")
+    def FEditMenu_init(self):
+        ## TODO : implement undo function
+        undoAction = QAction("Undo", self)
+        undoAction.setShortcut("Ctrl+Z")
+        undoAction.setStatusTip("Undoes the last action.")
+        undoAction.triggered.connect(lambda: self.print_stat_msg("Undo pressed."))
 
-        # print(json.dumps(self.File.getInfo(), indent = 4))
-        # print(self.File.__repr__())
+        ## TODO : implement redo function
+        redoAction = QAction("Redo", self)
+        redoAction.setShortcut("Ctrl+Y")
+        redoAction.setStatusTip("Redoes the last action.")
+        redoAction.triggered.connect(lambda: self.print_stat_msg("Redo pressed."))
 
-        # frame = file1.readFrame(46)
-        # frame.showImage()
-        # for i in range(file1.frameCount):
-        #     frame = file1.readFrame(i)
-        #     # frame.showImage()
-        #     ## uncomment the next part to save images on disk
-        #     image = frame.FRAME_DATA
-        #     image = np.array(image, dtype= np.uint8)
-        #     cv2.imwrite("frame_"+ str(i)+ "_data.jpg", image)
+        editMenu = self.mainMenu.addMenu("&Edit")
+        editMenu.addAction(undoAction)
+        editMenu.addAction(redoAction)
 
-    def loadFrameList(self):
-        """Function that loads frames before and after the current
-        Frame into the memory for faster processing.
-        Every time the user presses `Next` or `Previous` it modifies
-        the list to maintain the number of loaded frames.
-        
-            range: {integer} -- defines number of frames loaded into
-                    the memory.
 
-        """
-
-        framesIndices = list()
-        range = 10
-        if range > (self.File.FRAME_COUNT+1):
-            range = self.File.FRAME_COUNT
-        for i in range(range):
-            framesIndices.append()
-            
         pass
-"""
+    
+    def print_stat_msg(self, text):
+        print(text)
+
+    def FOpenFile(self):
+        self.FFilePath = QFileDialog.getOpenFileName(self, "Open File", "home/", "Sonar Files (*.aris *.ddf)")
+        print("file opened: "+ str(self.FFilePath))
+
 class FMainWindow(QDialog):
     """This class holds the main window which will be used to 
     show the SONAR images, analyze them and edit images.
@@ -170,7 +177,7 @@ class FMainWindow(QDialog):
     UI_FRAME_INDEX = 0
     FRAMES_LIST = list()
     SCALE = 2.0/3.0
-    def __init__(self):
+    def __init__(self, parent):
         """Initializes the window and loads the first frame and
         places the UI elements, each in its own place.
         """
@@ -183,8 +190,11 @@ class FMainWindow(QDialog):
 
         FNextBTN = QPushButton("Next",self)
         FNextBTN.clicked.connect(self.FShowNextImage)
+        FNextBTN.setShortcut(Qt.Key_Right)
+        
         FPreviousBTN = QPushButton("Previous",self)
         FPreviousBTN.clicked.connect(self.FShowPreviousImage)
+        FPreviousBTN.setShortcut(Qt.Key_Left)
 
         self.FLayout.addWidget(FNextBTN,1,1)
         self.FLayout.addWidget(FPreviousBTN,1,0)
@@ -298,13 +308,13 @@ class FMainWindow(QDialog):
             
         pass
 
-"""
+def run():
+    cwd = os.getcwd()
+    app = QApplication(sys.argv)
+    MainWindow = FWelcomeScreen()
+    sys.exit(app.exec_())
 
-cwd = os.getcwd()
-app = QApplication(sys.argv)
-MainWindow = FMainWindow()
-MainWindow.show()
-app.exec_()
+run()
 
 # # FFilePath = cwd + "/sample.aris"
 # self.FFilePath = "/home/mghobria/Documents/work/data/data 1/data.aris"
